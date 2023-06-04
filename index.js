927e70a9c2a539eee7b9fe3c060839646a7f8fdb
marceloanavia <marcelo.anavia@gmail.com>
2023-06-04 18:40:55 -0300

    add fetch

--- index.js
+++ index.js
@@ -10,6 +10,7 @@ window.addEventListener('DOMContentLoaded', function() {
                 DIV.innerHTML = `
                     <p>Nombre: ${persona.name}</p>
                     <p>Apellido: ${persona.lastName}</p>
+                    <p>Apellido: ${persona.age}</p>
                 `;
                 //Mejor forma de desarmar el objeto, se llama DESTRUCTURING ASSIGNMENT
                 /*const {name, lastName} = JSON.parse(xhr.responseText);
@@ -20,6 +21,19 @@ window.addEventListener('DOMContentLoaded', function() {
             }
         });
         xhr.send();
+    });
+
+    const BUTTON_FETCH = document.querySelector('#btn-fetch');
+    BUTTON_FETCH.addEventListener('click', function(){
+        fetch('ajax_json/persona.json') //este fetch() me va a devolver una promesa
+            .then(response => response.json()) //toma la respuesta "response" (promesa) y retornamos una nueva promesa pero que incluye el formato de respuesta de data como JSON. Then recibe la promesa, lo que pongo adentro se ejecuta cuando llega la respuesta
+            .then(data => {
+                DIV.innerHTML = `
+                    <p>fetch nombre = ${data.name}</p>
+                    <p>fetch nombre = ${data.lastName}</p>
+                    <p>fetch nombre = ${data.age}</p>
+                `;
+            });
     });
     
 });
